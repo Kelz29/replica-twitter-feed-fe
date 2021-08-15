645e6df8ed38f973a533db8c7231bf171e704c7f
kelz29 <makamanekj@gmail.com>
2021-08-15 21:10:31 +0200

    chore: deployed link

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 export const GET_TWEETS_ENDPOINT = "get-tweets";
 export const API_URL_DEV = `http://localhost:3000/${GET_TWEETS_ENDPOINT}`;
-export const API_URL_DEPLOYED = "http://some-url";
+export const API_URL_DEPLOYED = `https://automatic-same-abacus.glitch.me/get-tweets`;
